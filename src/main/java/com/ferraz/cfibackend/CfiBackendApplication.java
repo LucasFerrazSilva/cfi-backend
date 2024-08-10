515681e77365da24d4a44c7a8fdacf5c7d0b57c1
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-08-09 21:51:16 -0300

    first commit

---
+++ src/main/java/com/ferraz/cfibackend/CfiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ferraz.cfibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferraz/cfibackend/CfiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferraz.cfibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
